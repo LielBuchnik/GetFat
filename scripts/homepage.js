356c841986ce5d54af54bdeadb453d77e5dcc4d3
Liel Buchnik <102684504+LielBuchnik@users.noreply.github.com>
2023-11-09 15:26:18 +0200

    Update 9.11.23
    Updated main.css
    assets
    hompage.html
    homepage.js

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -25,10 +25,11 @@ welcomeUser()
 function welcomeUser(){
     let userD = localStorage.getItem("userDetails")
     userD = JSON.parse(userD);
-    welcomeU.innerHTML = `Hey ${userD.username}, Welcome back!`
+    welcomeU.innerHTML = `Hey ${userD.username}`
 }
 
 function getRests() {
+    restGrid.innerHTML ='';
     for (let i = 0; i < restList.length; i++) {
         let rest = restList[i];
         console.log(restList[i])
@@ -49,10 +50,20 @@ function showMore_handler(index) {
     window.location.href = `../html/restpage.html?id=${index}`
 }
 
-// function sortByFood() {
-//     let priorityOrder = ["Pizza", "Middle East", "Asian", "Mexican", "Burgers"];
-//     restGrid.sort((a, b) => {
-//         return priorityOrder.indexOf(a.priority) - priorityOrder.indexOf(b.priority);
-//     });
-//     getRests();
-// }
+function sortByFood(event, foodType) {
+    let filteredRestaurants = restList.filter(rest => rest.type === foodType);
+    restGrid.innerHTML = "";
+
+    for (let i = 0; i < filteredRestaurants.length; i++) {
+        let rest = filteredRestaurants[i];
+        restGrid.innerHTML +=
+            `<div class="rest-box" onclick="showMore_handler(${i})">
+                <img class="rest-image" src="${rest.restImg}">
+                <p>${rest.name}</p>
+                <p class="rest-type">${rest.type}</p>
+                <p class="rest-rating">${rest.rating} <i class='bx bx-star'></i></p>
+                <p class="rest-desc">${rest.description}</p>
+                <p class="rest-id">${rest.id}</p>
+            </div>`;
+    }
+}
